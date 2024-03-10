99078807e59927b3d609a6e205e7b2a5ee40021e
kasun beddewela <kbeddewela@aampglobal.com>
2024-03-10 14:48:29 -0400

    still working on the GPIO pin asignments for the PCB bring up

--- 16f877_bringup.X/System/GPIO_driver.c
+++ 16f877_bringup.X/System/GPIO_driver.c
@@ -10,6 +10,55 @@
 #include <xc.h>
 #include <pic16f877a.h>
 
+/* PORT A pin assignments 
+ * RA0 ANLG_X1 analog stick X1 of JOY1
+ * RA1 ANLG_Y1 analog stick Y1 of JOY1
+ * RA2 ANLG_X2 analog stick X2 of JOY2
+ * RA3 ANLG_Y2 analog stick Y2 of JOY2
+ * 
+ * RA4 clk3 we made big mistake the RA4 only supports pulldown, we forgot to put
+ * a pullup resostor for clock 3
+ * RA5 CS or SS of SPI in communication header J1
+ */
+
+/* PORT B pin assignments 
+ * PB0 BUTTON UP
+ * PB1 BUTTON DOWN
+ * PB2 BUTTON LEFT
+ * PB3 BUTTON RIGHT
+ * PB4 BUTTON A
+ * PB5 BUTTON B
+ * PB6 
+ * PB7 THUMB BUTTON
+ * 
+ */
+
+/* PORT C pin assignment 
+ * RC0 Clk4
+ * RC1 BUZZER1
+ * RC2 BUZZER2
+ * RC3 SCL
+ * RC4 SDA
+ * RC5 SDO
+ * RC6 TX_UART
+ * RC7 RX_UART
+ */
+
+/*PORT D pin assignment 
+ * RE0 Row1
+ * RE1 Row2
+ * RE2 Row3
+ * RE3 Row4
+ * RE4 Row5
+ * RE5 Row6
+ * RE6 Row7
+ * Re7 Row8
+ */
+/* PORT E pin assignment
+ * RE0 clk1
+ * RE1 clk2
+ * RE2 reset
+ */
 void PORTA_config()
 {
     /* first we set all the outputs to 0
@@ -22,15 +71,16 @@ void PORTA_config()
      *  */
     
     //TRISA = 0x1F;/* tris  a is also 5 bits long  we can use 0b11111 notation also*/
-    TRISAbits.TRISA0 = 0;
-    TRISAbits.TRISA1 = 0;
-    TRISAbits.TRISA2 = 0;
-    TRISAbits.TRISA3 = 0;
-    TRISAbits.TRISA4 = 0;
-    TRISAbits.TRISA5 = 0;
+    TRISAbits.TRISA0 = 1; /* TODO:analog input */
+    TRISAbits.TRISA1 = 1; /* TODO:analog input */
+    TRISAbits.TRISA2 = 1; /* TODO:analog input*/
+    TRISAbits.TRISA3 = 1; /* TODO:analog input */
+    TRISAbits.TRISA4 = 0; /* digital output clk3 */
+    TRISAbits.TRISA5 = 0; /*TODO digital out SPI CS */
     
     /* the ADCCON1's bits 0 to 4 handles the ADC configration*/
-    ADCON1 = 0x06; /* PCF<3:0> = 0b011x means all are DIGITAL pins*/ 
+    /* PCF<3:0> = 0b011x means all are DIGITAL pins*/
+    ADCON1 = 0x06;  /* TODO: we change this when we start work on the joy stick*/
    
     /* TODO: PORTA 4, aka RA4 pin need configuration 
      * of the CMCON and CVRCON  registers  also 
@@ -44,6 +94,7 @@ void PORTA_config()
      * CM2:CM0 bits are 111 at default reset so that means they
      * do not effect the output of the RA4 and RA5 pins*/
 }
+
 
 
 void PORTB_config()
